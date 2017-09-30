ecb8463d19513bb6b64e39a530be9a7e00c6a41a
Julian Nymark <julianymark@gmail.com>
2017-09-30 16:52:10 +0200

    better

--- vsc/setup.sh
+++ vsc/setup.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# this script will install vsc extensions
+# this script will:
+# 1. try to install vsc extensions
+# 2. copy keybindings.json & settings.json into your user preferences (no overwrite!)
 
-# code --list-extensions
+############################
+# try to install extentions
+############################
 
 declare -a arr=(
     "JPTarquino.postgresql"
@@ -19,3 +23,10 @@ for e in "${arr[@]}"
 do
    code --install-extension "$e"
 done
+
+############################
+# copy user preferences (-n no clobber!)
+############################
+
+cp -n ./settings.json "$HOME/.config/Code/User/settings.json"
+cp -n ./keybindings.json "$HOME/.config/Code/User/keybindings.json"
