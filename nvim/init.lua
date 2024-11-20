64b108d94b680220cde6ee0b67f09494cb4e93d0
Julian Nymark <j@juliannymark.com>
2024-11-21 00:44:21 +0100

    :art: silence the "nothing" messages

--- nvim/init.lua
+++ nvim/init.lua
@@ -1100,6 +1100,9 @@ require("lazy").setup({
 					["vim.lsp.util.stylize_markdown"] = true,
 					["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
 				},
+				hover = {
+					silent = true,
+				},
 			},
 			-- you can enable a preset for easier configuration
 			presets = {
