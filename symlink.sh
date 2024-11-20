ebaa92cd2be605746252f2135209da0768fa4546
Julian Nymark <julian.nymark@nav.no>
2024-11-20 09:10:42 +0100

    :bug: symlink fixes

--- symlink.sh
+++ symlink.sh
@@ -1,4 +1,5 @@
 # running this file assumes you don't have existing files
-ln -s $(pwd)/nvim ~/.config/nvim
+ln -s $(pwd)/nvim ~/.config
 ln -s $(pwd)/tmux/.tmux.conf ~/.tmux.conf
+mkdir -p ~/.config/tms
 ln -s $(pwd)/tmux-sessionizer/config.toml ~/.config/tms/config.toml
