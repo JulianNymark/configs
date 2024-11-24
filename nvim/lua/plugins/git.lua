55e7fd5e82703154c7e5dc7d4187de7f5f55ff69
Julian Nymark <j@juliannymark.com>
2024-11-24 02:00:44 +0100

    :art: git tweaks

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -20,6 +20,8 @@ return {
 						vim.keymap.set(mode, l, r, opts)
 					end
 
+					gitsigns.toggle_current_line_blame()
+
 					-- Navigation
 					map("n", "]c", function()
 						if vim.wo.diff then
@@ -49,7 +51,7 @@ return {
 					map("n", "<leader>hS", gitsigns.stage_buffer, { desc = "[h]unk [S]tage buffer" })
 					map("n", "<leader>hu", gitsigns.undo_stage_hunk, { desc = "[h]unk [u]ndo" })
 					map("n", "<leader>hR", gitsigns.reset_buffer, { desc = "[h]unk [R]eset buffer" })
-					map("n", "<leader>hp", gitsigns.preview_hunk, { desc = "[h]unk [p]review hunk" })
+					map("n", "<leader>hp", gitsigns.preview_hunk_inline, { desc = "[h]unk [p]review hunk" })
 					map("n", "<leader>hb", function()
 						gitsigns.blame_line({ full = true })
 					end, { desc = "[h]unk [b]lame line" })
@@ -69,36 +71,6 @@ return {
 					map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", { desc = "gitsigns select_hunk" })
 				end,
 			}, opts))
-		end,
-	},
-	{
-		"tanvirtin/vgit.nvim",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-		},
-		config = function() -- This is the function that runs, AFTER loading
-			local vgit = require("vgit")
-			vgit.setup({})
-			vim.keymap.set("n", "<C-k>", vgit.hunk_up, { desc = "hunk up" })
-			vim.keymap.set("n", "<C-j>", vgit.hunk_down, { desc = "hunk down" })
-			vim.keymap.set("n", "<leader>gs", vgit.buffer_hunk_stage, { desc = "buffer hunk [s]tage" })
-			vim.keymap.set("n", "<leader>gr", vgit.buffer_hunk_reset, { desc = "buffer hunk [r]eset" })
-			vim.keymap.set("n", "<leader>gp", vgit.buffer_hunk_preview, { desc = "buffer hunk [p]review" })
-			vim.keymap.set("n", "<leader>gb", vgit.buffer_blame_preview, { desc = "buffer [b]lame preview" })
-			vim.keymap.set("n", "<leader>gf", vgit.buffer_diff_preview, { desc = "buffer di[f]f preview" })
-			vim.keymap.set("n", "<leader>gh", vgit.buffer_history_preview, { desc = "buffer hunk [h]istory preview" })
-			vim.keymap.set("n", "<leader>gu", vgit.buffer_reset, { desc = "buffer [u]ndo all" })
-			vim.keymap.set(
-				"n",
-				"<leader>gg",
-				vgit.buffer_gutter_blame_preview,
-				{ desc = "[g]utter buffer blame preview" }
-			)
-			vim.keymap.set("n", "<leader>glu", vgit.project_hunks_preview, { desc = "[u]nstaged" })
-			vim.keymap.set("n", "<leader>gls", vgit.project_hunks_staged_preview, { desc = "[l]ist [s]taged" })
-			vim.keymap.set("n", "<leader>gd", vgit.project_diff_preview, { desc = "[d]iff" })
-			vim.keymap.set("n", "<leader>gq", vgit.project_hunks_qf, { desc = "[q]uickfix hunks" })
-			vim.keymap.set("n", "<leader>gx", vgit.toggle_diff_preference, { desc = "toggle betwi[x]t diff pref" })
 		end,
 	},
 }
