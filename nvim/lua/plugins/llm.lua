0cf4c0a616c89d8e3cd8017790732321ae3251ec
Julian Nymark <j@juliannymark.com>
2025-02-22 17:46:59 +0100

    llm ollama config

--- nvim/lua/plugins/llm.lua
+++ nvim/lua/plugins/llm.lua
@@ -9,13 +9,32 @@ return {
 		vendors = {
 			self_hosted = {
 				__inherited_from = "openai",
-				endpoint = "http://127.0.0.1:1234/v1",
-				model = "whatever_hosted_on_port_123",
+				endpoint = "http://localhost:11434/v1",
+				model = "qwen2.5-coder:7b",
 				timeout = 30000, -- timeout in milliseconds
 				temperature = 0, -- adjust if needed
 				max_tokens = 4096,
 				-- reasoning_effort = "high", -- only supported for "o" models
 				api_key_name = "",
+				parse_curl_args = function(opts, code_opts)
+					return {
+						url = opts.endpoint .. "/chat/completions",
+						headers = {
+							["Accept"] = "application/json",
+							["Content-Type"] = "application/json",
+							["x-api-key"] = "ollama",
+						},
+						body = {
+							model = opts.model,
+							messages = require("avante.providers").copilot.parse_messages(code_opts), -- you can make your own message, but this is very advanced
+							max_tokens = 2048,
+							stream = true,
+						},
+					}
+				end,
+				parse_response_data = function(ctx, data_stream, event_state, opts)
+					require("avante.providers").openai.parse_response(ctx, data_stream, event_state, opts)
+				end,
 			},
 		},
 		mappings = {
