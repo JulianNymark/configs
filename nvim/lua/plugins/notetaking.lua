819388ea942b99a265f7593425a17c82f195496f
Julian Nymark <julian.nymark@nav.no>
2025-05-12 10:55:27 +0200

    use hotfix for zk-nvim

--- nvim/lua/plugins/notetaking.lua
+++ nvim/lua/plugins/notetaking.lua
@@ -1,6 +1,9 @@
 return {
 	{
-		"zk-org/zk-nvim",
+		-- "zk-org/zk-nvim",
+		"juliannymark/zk-nvim",
+		branch = "add-some-guards",
+
 		config = function()
 			require("zk").setup({
 				picker = "telescope",
