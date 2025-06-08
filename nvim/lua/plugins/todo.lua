009fbe85f884d021428645cca21a85026fb9a06e
Julian Nymark <j@juliannymark.com>
2025-06-08 11:51:22 +0200

    colors todo-comments

--- nvim/lua/plugins/todo.lua
+++ nvim/lua/plugins/todo.lua
@@ -3,5 +3,15 @@ return {
 	"folke/todo-comments.nvim",
 	event = "VimEnter",
 	dependencies = { "nvim-lua/plenary.nvim" },
-	opts = { signs = false },
+	opts = {
+		signs = false,
+		colors = {
+			error = { "DiagnosticError", "ErrorMsg", "#DC2626" },
+			warning = { "DiagnosticWarn", "WarningMsg", "#FBBF24" },
+			info = { "DiagnosticInfo", "#2563EB" },
+			hint = { "#10B981" },
+			default = { "Identifier", "#7C3AED" },
+			test = { "Identifier", "#FF00FF" },
+		},
+	},
 }
