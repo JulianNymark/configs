7b4d86e9445f51ca8e053ec3da118f1502e20b89
Julian Nymark <julian.nymark@nav.no>
2025-06-26 15:30:01 +0200

    only disable LSP format for lua (it gets tabs?!) .gitignore

--- nvim/lua/plugins/autoformat.lua
+++ nvim/lua/plugins/autoformat.lua
@@ -26,11 +26,11 @@ return {
       -- Disable "format_on_save lsp_fallback" for languages that don't
       -- have a well standardized coding style. You can add additional
       -- languages here or re-enable it for the disabled ones.
-      local disable_filetypes = { c = true, cpp = true }
+      local disable_filetypes = { c = true, cpp = true, lua = true }
       return {
         timeout_ms = 500,
-        lsp_format = "prefer",
-        -- lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+        -- lsp_format = "prefer",
+        lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
       }
     end,
     formatters_by_ft = {
