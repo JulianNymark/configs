9f32749007eacbeee6e7f2a206433bc7faa4cb33
Julian Nymark <julian.nymark@nav.no>
2025-06-26 15:58:49 +0200

    prettier config for autoformat

--- nvim/lua/plugins/autoformat.lua
+++ nvim/lua/plugins/autoformat.lua
@@ -14,7 +14,7 @@ return {
     {
       "<leader>fF",
       function()
-        require("conform").format({ async = true })
+        require("conform").format({ async = true, lsp_format = "fallback" })
       end,
       mode = "",
       desc = "[F]ormat without LSP",
@@ -23,36 +23,28 @@ return {
   opts = {
     notify_on_error = false,
     stop_after_first = true,
+    lsp_format = "fallback",
     format_on_save = function(bufnr)
-      -- Disable "format_on_save lsp_fallback" for languages that don't
-      -- have a well standardized coding style. You can add additional
-      -- languages here or re-enable it for the disabled ones.
-      local prefer_lsp = { c = true, cpp = true, lua = true }
-      local function get_lsp_fallback()
-        if prefer_lsp[vim.bo[bufnr].filetype] then
-          return "prefer" -- soft "only LSP"
-        end
-        return "fallback" -- soft "no LSP" LSP "if nothing else"
-      end
       return {
         timeout_ms = 500,
-        stop_after_first = true,
-        lsp_format = get_lsp_fallback(),
       }
     end,
     formatters_by_ft = {
-      lua = { "stylua" },
-      javascript = { "prettierd", "eslint", stop_after_first = true },
-      javascriptreact = { "prettierd", "eslint", stop_after_first = true },
-      typescript = { "prettierd", "eslint", stop_after_first = true },
-      typescriptreact = { "prettierd", "eslint", stop_after_first = true },
+      c = { lsp_format = "prefer" },
+      cpp = { lsp_format = "prefer" },
+      lua = { "stylua", lsp_format = "prefer" },
 
-      css = { "prettierd", "eslint", stop_after_first = true },
-      scss = { "prettierd", "eslint", stop_after_first = true },
-      json = { "prettierd", "eslint", stop_after_first = true },
-      svelte = { "prettierd", "eslint", stop_after_first = true },
-      graphql = { "prettierd", "eslint", stop_after_first = true },
-      markdown = { "prettierd", "eslint", stop_after_first = true },
+      javascript = { "prettierd", "eslint" },
+      javascriptreact = { "prettierd", "eslint" },
+      typescript = { "prettierd", "eslint" },
+      typescriptreact = { "prettierd", "eslint" },
+
+      css = { "prettierd", "eslint" },
+      scss = { "prettierd", "eslint" },
+      json = { "prettierd", "eslint" },
+      svelte = { "prettierd", "eslint" },
+      graphql = { "prettierd", "eslint" },
+      markdown = { "prettierd", "eslint" },
     },
   },
 }
