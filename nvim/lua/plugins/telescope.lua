a8aa118c3b9e40579c2c5d35d5c721f9ec4cc478
Julian Nymark <j@juliannymark.com>
2025-06-28 23:45:42 +0200

    use the custom keymaps

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -103,7 +103,7 @@ return {
       local builtin = require("telescope.builtin")
 
       vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[h]elp" })
-      vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[k]eymaps" })
+      -- vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[k]eymaps" })
       vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[f]iles" })
       vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[s]elect Telescope" })
       vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "current [w]ord" })
@@ -189,11 +189,11 @@ return {
     opts = {},
     keys = {
       {
-        "<leader>sK",
+        "<leader>sk",
         function()
           require("telescope-grouped-keymaps").picker_grouped_keymaps({})
         end,
-        desc = "search grouped keymaps"
+        desc = "[k]eymaps"
       }
     },
   },
