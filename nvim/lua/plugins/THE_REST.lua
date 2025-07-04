a0b12849fd029fbabe7ca254c12fd29e8bc2053b
Julian Nymark <j@juliannymark.com>
2025-07-04 14:33:37 +0200

    nvim SpOnGeBoB

--- nvim/lua/plugins/THE_REST.lua
+++ nvim/lua/plugins/THE_REST.lua
@@ -4,6 +4,15 @@ return {
       "ThePrimeagen/vim-be-good",
     },
     {
+      "tjdevries/sPoNGe-BoB.NvIm",
+      config = function(_, opts)
+        local spongebob = require("sponge-bob")
+        vim.keymap.set("n", "<leader>t?", function()
+          spongebob.toggle()
+        end, { desc = "sPoNgEbOb" })
+      end
+    },
+    {
       "folke/twilight.nvim",
       opts = {
         context = 0,
