5df5cde9105d5ee87725b24d098ee7773a4b64b0
Julian Nymark <j@juliannymark.com>
2025-07-15 16:33:27 +0200

    dialogue files

--- nvim/lua/filetype.lua
+++ nvim/lua/filetype.lua
@@ -1,5 +1,16 @@
+vim.filetype.add({
+  extension = {
+    dialogue = "dialogue"
+  }
+})
+
 -- this is for non-LSP setup,
 -- this gets overriden if the LSP controls any behaviour
+-- also, .editorconfig will override this as well
+
+-- handy for debugging
+-- :verbose set expandtab?
+
 vim.api.nvim_create_autocmd("FileType", {
   pattern = { "gdscript", "python" },
   callback = function()
@@ -7,5 +18,16 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.bo.tabstop = 4      -- Number of spaces a <Tab> counts for
     vim.bo.shiftwidth = 4   -- Number of spaces for each indent
     vim.bo.softtabstop = 4  -- Number of spaces for <Tab> in insert mode
+  end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "dialogue" },
+  callback = function()
+    vim.print("dialogue file")
+    vim.bo.expandtab = false
+    vim.bo.tabstop = 4     -- Number of spaces a <Tab> counts for
+    vim.bo.shiftwidth = 4  -- Number of spaces for each indent
+    vim.bo.softtabstop = 4 -- Number of spaces for <Tab> in insert mode
   end,
 })
