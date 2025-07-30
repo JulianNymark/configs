f6f633d55a338c9e4ccb046e00e5afdcb14ccc51
Julian Nymark <j@juliannymark.com>
2025-07-30 15:17:59 +0300

    zk aliases and completions

--- zsh/completions/_zk.zsh
+++ zsh/completions/_zk.zsh
@@ -1,7 +1,10 @@
 #compdef zk
 
 _zk() {
-  _values options daily journal edlast recent lucky ls t config daily journal n ni standup ls-standup ideas update rm slides
+  _values options \
+    lucky config n ni daily standup ls recent journal \
+    ls-standup edlast t ideas update rm slides wc bl log \
+    sync
 }
 
 _zk "$@"
