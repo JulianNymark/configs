ae560264fe565f4122ca70fd9d061a23a23e3fe7
Julian Nymark <julian.nymark@nav.no>
2025-09-16 10:26:28 +0200

    try out orgmode.nvim

--- nvim/lua/plugins/THE_REST.lua
+++ nvim/lua/plugins/THE_REST.lua
@@ -18,5 +18,16 @@ return {
         context = 0,
       },
     },
+    {
+      'nvim-orgmode/orgmode',
+      event = 'VeryLazy',
+      config = function()
+        -- Setup orgmode
+        require('orgmode').setup({
+          org_agenda_files = '~/orgfiles/**/*',
+          org_default_notes_file = '~/orgfiles/refile.org',
+        })
+      end,
+    }
   },
 }
