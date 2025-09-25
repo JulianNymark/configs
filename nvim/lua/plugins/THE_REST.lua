dddea5a5b4937a73296a9d351ad6cd360c25f5c4
Julian Nymark <julian.nymark@nav.no>
2025-09-25 16:42:38 +0200

    try rest.nvim

--- nvim/lua/plugins/THE_REST.lua
+++ nvim/lua/plugins/THE_REST.lua
@@ -1,33 +1,44 @@
 return {
   {
-    {
-      "ThePrimeagen/vim-be-good",
+    "ThePrimeagen/vim-be-good",
+  },
+  {
+    "tjdevries/sPoNGe-BoB.NvIm",
+    config = function(_, opts)
+      local spongebob = require("sponge-bob")
+      vim.keymap.set("n", "<leader>t?", function()
+        spongebob.toggle()
+      end, { desc = "sPoNgEbOb" })
+    end
+  },
+  {
+    "folke/twilight.nvim",
+    opts = {
+      context = 0,
     },
-    {
-      "tjdevries/sPoNGe-BoB.NvIm",
-      config = function(_, opts)
-        local spongebob = require("sponge-bob")
-        vim.keymap.set("n", "<leader>t?", function()
-          spongebob.toggle()
-        end, { desc = "sPoNgEbOb" })
-      end
-    },
-    {
-      "folke/twilight.nvim",
-      opts = {
-        context = 0,
-      },
-    },
-    {
-      'nvim-orgmode/orgmode',
-      event = 'VeryLazy',
-      config = function()
-        -- Setup orgmode
-        require('orgmode').setup({
-          org_agenda_files = '~/orgfiles/**/*',
-          org_default_notes_file = '~/orgfiles/refile.org',
-        })
+  },
+  {
+    'nvim-orgmode/orgmode',
+    event = 'VeryLazy',
+    config = function()
+      -- Setup orgmode
+      require('orgmode').setup({
+        org_agenda_files = '~/orgfiles/**/*',
+        org_default_notes_file = '~/orgfiles/refile.org',
+      })
+    end,
+  },
+  {
+    "rest-nvim/rest.nvim",
+    -- also some dependencies specified in packages.lua...
+    -- some luarocks stuff... it's seemingly kinda jank
+    -- origin of hack: https://github.com/rest-nvim/rest.nvim/issues/306
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      opts = function(_, opts)
+        opts.ensure_installed = opts.ensure_installed or {}
+        table.insert(opts.ensure_installed, "http")
       end,
     }
-  },
+  }
 }
