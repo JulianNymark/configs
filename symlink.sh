b3a73d84de91b24f5a1915fcfb6b15dc5112e81a
Julian Nymark <j@juliannymark.com>
2025-10-11 01:24:30 +0200

    add ghostty

--- symlink.sh
+++ symlink.sh
@@ -3,5 +3,6 @@ ln -fs $(pwd)/zk ~/.config
 ln -fs $(pwd)/yazi ~/.config
 ln -fs $(pwd)/zsh ~/.config
 ln -fs $(pwd)/tmux/.tmux.conf ~/.tmux.conf
+ln -fs $(pwd)/ghostty ~/.config
 mkdir -p ~/.config/tms
 ln -fs $(pwd)/tmux-sessionizer/config.toml ~/.config/tms/config.toml
